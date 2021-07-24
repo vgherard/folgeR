6ecf0ef104cab67eebf329c3977ee6a00752a92e
vgherard <vgherard@sissa.it>
2021-07-24 13:40:54 +0200

    Add helper for constructing URL requests.

---
+++ R/utils.R
@@ -0,0 +1,13 @@
+#' @title Construct Folger URL request
+#'
+#' @description Helper for constructing URL requests to the Folger API.
+#'
+#' @param play A length one character. Code of the requested play.
+#' @param fun API function.
+#' @param ... not used.
+#'
+#' @return a character vector. URL request to the Folger API.
+#'
+#' @noRd
+folg_url_req <- function(play, fun, ...)
+        paste0("https://www.folgerdigitaltexts.org/", play, "/", fun, "/")
